e30e71e1f4fba291389141cbcd84ef9c02be50cc
Jonn Dillon <jonn_dillon@iinet.net.au>
2018-03-01 23:02:35 +1000

    Fixed merge issues and file name resolving

--- scripts/copy.js
+++ scripts/copy.js
@@ -2,18 +2,14 @@
 
 const cpy = require('cpy');
 
-cpy(['src/docs/public/*','!src/public/images','!src/public/favicon'], 'public').then(() => {
-    console.log('Public files copied from SRC to DIST complete');
-});
-
-cpy('src/docs/public/favicon/*', 'public/favicon').then(() => {
+cpy('src/public/favicon/*', 'public/favicon').then(() => {
     console.log('Public (Favicon) files copied from SRC to DIST complete');
 });
 
-cpy('src/docs/public/images/*', 'public/images').then(() => {
+cpy('src/public/images/*', 'public/images').then(() => {
     console.log('Public (Images) files copied from SRC to DIST complete');
 });
 
-cpy('src/docs/config/*', 'config').then(() => {
+cpy('src/config/*', 'config').then(() => {
     console.log('Config files copied from SRC to DIST complete');
 });
